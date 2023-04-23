c5dfc5a52e5c81142e7cf8b515e66cd3a8e5f491
up <zhiboc@andrew.cmu.edu>
2023-04-23 16:35:42 -0600

    (8/15)基本完成代码生成，调试

--- runtime/files/libuv/filesystem.c
+++ runtime/files/libuv/filesystem.c
@@ -44,6 +44,61 @@ yy_ptr yyReadFileSync(yy_ptr filenamearg) {
 }
 
 
+yy_ptr yyWriteFileSync(yy_ptr file_name_addr, yy_ptr content_addr) {
+    const char *filename = addr_to_string(file_name_addr);
+    const char *content = addr_to_string(content_addr);
+    uv_loop_t *loop = uv_default_loop();
+
+    // Create a new uv_fs_t structure for creating the directory
+    uv_fs_t mkdir_req;
+    memset(&mkdir_req, 0, sizeof(uv_fs_t));
+
+    // Create the directory with permissions 0755
+    int result = uv_fs_mkdir(loop, &mkdir_req, filename, 0755, NULL);
+    if (result == UV_EEXIST) {
+        // Directory already exists
+    } else if (result != 0) {
+        // Failed to create directory
+        fprintf(stderr, "Error creating directory: %s\n", uv_strerror(result));
+        return;
+    }
+
+    // Create a new uv_fs_t structure for writing the file
+    uv_fs_t open_req;
+    memset(&open_req, 0, sizeof(uv_fs_t));
+
+    // Open the file for writing, with permissions 0644
+    result = uv_fs_open(loop, &open_req, filename, O_CREAT | O_WRONLY | O_TRUNC, 0644, NULL);
+    if (result < 0) {
+        // Failed to open file
+        fprintf(stderr, "Error opening file: %s\n", uv_strerror(result));
+        return;
+    }
+
+    // Write the content to the file
+    uv_fs_t write_req;
+    memset(&write_req, 0, sizeof(uv_fs_t));
+    uv_buf_t buf = uv_buf_init((char*) content, strlen(content));
+    result = uv_fs_write(loop, &write_req, result, &buf, 1, -1, NULL);
+    if (result < 0) {
+        // Failed to write to file
+        fprintf(stderr, "Error writing to file: %s\n", uv_strerror(result));
+        return;
+    }
+
+    // Close the file
+    uv_fs_t close_req;
+    memset(&close_req, 0, sizeof(uv_fs_t));
+    result = uv_fs_close(loop, &close_req, result, NULL);
+    if (result < 0) {
+        // Failed to close file
+        fprintf(stderr, "Error closing file: %s\n", uv_strerror(result));
+        return;
+    }
+
+    return unit_to_addr();
+}
+
 yy_ptr yyListDirectorySync(yy_ptr dirname) {
     uv_fs_t scan_req;
     // uv_fs_t read_req;
