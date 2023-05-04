feba13d3e535595bd9db5285af34990a22b5d89d
up <zhiboc@andrew.cmu.edu>
2023-05-04 10:21:48 -0600

    save

--- runtime/files/libuv/filesystem.c
+++ runtime/files/libuv/filesystem.c
@@ -227,6 +227,22 @@ yy_ptr yyIsPathRegularFile(yy_ptr path){
   return bool_to_addr(isfile);
 }
 
+yy_ptr yyGetFileModifiedTime(yy_ptr path){
+
+    uv_fs_t req;
+
+    char * pathC = addr_to_string(path);
+
+  int r = uv_fs_stat(NULL, &req, pathC, NULL);
+  if (r != 0){
+      fprintf(stderr, " cannot stat file");
+      errorAndAbort("cannot stat file!!!");
+  }
+  int64_t mtime = (((uv_stat_t*)req.ptr)->st_mtim).tv_sec;
+
+  return int_to_addr(mtime);
+}
+
 yy_ptr yyPathExists(yy_ptr path){
     uv_fs_t req;
 
