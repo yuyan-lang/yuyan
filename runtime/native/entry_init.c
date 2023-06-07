4ed97b32a48dc29d6bcf45dd1bc09dccbb0f42ff
zc <zhiboc@andrew.cmu.edu>
2023-06-07 00:22:51 -0400

    save temp changes

--- runtime/native/entry_init.c
+++ runtime/native/entry_init.c
@@ -7,7 +7,8 @@ uv_loop_t *uv_global_loop;
 void optional_entry_initialization(){
         // initialize garbage collection
     GC_INIT();
-    
+    GC_enable_incremental();
+    GC_expand_hp(60719476736); // about 60 GB   
         // initialize uv default loop (can replace)
     uv_global_loop = uv_default_loop();
 
