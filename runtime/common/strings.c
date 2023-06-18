e4dd27cec2c2d4925bfb347c3c725a79d31c9586
zc <zhiboc@andrew.cmu.edu>
2023-06-18 13:53:28 -0400

    save

--- runtime/common/strings.c
+++ runtime/common/strings.c
@@ -325,5 +325,8 @@ yy_ptr yyCodePointsConcat(yy_ptr str_list_addr) {
 
     free(lengths);
 
+    // for debugging
+    printf("result string is %s", resultString);
+
     return string_to_addr(resultString);
 }
