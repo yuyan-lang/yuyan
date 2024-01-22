86cda81a9055b6424f5121b82f73992451940964
up <zhiboc@andrew.cmu.edu>
2024-01-22 15:18:02 -0500

    fix exception handling

--- 运行时支持库/native/exception.c
+++ 运行时支持库/native/exception.c
@@ -25,32 +25,30 @@ uint64_t errorAndAbort(char* errMsg){
 }
 
 // I believe abssingle has two arguments , the first is just the closure itself
-uint64_t 全局异常处理器(yyvalue stack_top, yyvalue cont_id, yyvalue argument, yyvalue return_record){
+yyvalue 全局异常处理器(yyvalue stack_top, yyvalue cont_id, yyvalue argument, yyvalue return_record){
     fprintf(stderr, "！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！\n豫言运行环境(yy_runtime)：未捕捉的异常(Uncaught Exception)：\n");
     fprintf(stderr, "尝试打印值：（可能会出现 异常）：\n");
     fflush(stderr);
-    fprintf(stderr, "%s", *((char **)(argument) + 1));
+    fprintf(stderr, "%s", yyvalue_to_string(yy_read_tuple(argument, 1)));
     fprintf(stderr, "\n");
     exit(1);
-    return -1;
+    return unit_to_yyvalue();
 }
 
 
-void* 当前异常处理器;
-extern void* 内部全局异常处理器(void* arg1);
+yyvalue 当前异常处理器;
 
 
 void yy_豫言初始化全局异常处理器(){
-    void **tup = (void**) yy_gcAllocateArray(2);
-    tup[0] = 全局异常处理器;
-    当前异常处理器 = tup;
+    当前异常处理器 = tuple_to_yyvalue(2, (yyvalue[]){
+        funcptr_to_yyvalue(全局异常处理器), unit_to_yyvalue() });
 }
 
-void* 获取当前异常处理器() {
+yyvalue 获取当前异常处理器() {
     return 当前异常处理器;
 }
  
-void* 设置当前异常处理器(void* 处理器) {
+yyvalue 设置当前异常处理器(yyvalue 处理器) {
     当前异常处理器 = 处理器;
-    return 0;
+    return unit_to_yyvalue();
 }
