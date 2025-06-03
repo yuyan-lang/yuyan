cf54149d04d9f3ddbde600fa2d790c2a3123954a
up <zhiboc@andrew.cmu.edu>
2025-06-02 20:23:02 -0400

    save

--- yyocamlc/lib/ocaml_output/OcamlOutput.ml
+++ yyocamlc/lib/ocaml_output/OcamlOutput.ml
@@ -20,13 +20,44 @@ let convert_to_ocaml_identifier_no_uid (input: string) : string =
 let get_module_name_from_filepath (filepath : string) : string = 
   "M_" ^ (convert_to_ocaml_identifier_no_uid (filepath))
 
+let get_identifier_name (expr : A.t) : string = 
+  match A.view expr with
+  | A.FreeVar(name) -> "v_" ^ convert_to_ocaml_identifier_no_uid name
+  | _ -> failwith ("OO33: Expecting free variable, got: " ^ A.show_view expr)
+
+
 let get_ocaml_code (expr : A.t) : string = 
   match A.view expr with
-  | _ -> failwith ("OO5: OCaml output not implemented for expression: " ^ A.show_view expr)
+  | A.N(N.Builtin(StringType), []) -> "Builtin.stringType"
+  | A.N(N.Builtin(IntType), []) -> "Builtin.intType"
+  | A.N(N.Builtin(BoolType), []) -> "Builtin.boolType"
+  | A.N(N.Builtin(UnitType), []) -> "Builtin.unitType"
+  | A.N(N.Builtin(FloatType), []) -> "Builtin.floatType"
+  | A.N(N.Builtin(Type), []) -> "Builtin.typeType"
+  | A.N(N.Builtin(N.Int i ), []) -> "Builtin.intValue " ^ string_of_int i
+  | A.N(N.Builtin(N.Float(int_part,decimal_part)), []) -> "Builtin.floatValue " ^ int_part ^ "." ^ decimal_part
+  | A.N(N.Builtin(N.String s ), []) -> "Builtin.stringValue " ^ "\"" ^ String.escaped s ^ "\""
+  | A.N(N.Builtin(N.Unit), []) -> "Builtin.unitValue"
+  | A.N(N.Builtin(N.Bool true), []) -> "Builtin.trueValue"
+  | A.N(N.Builtin(N.Bool false), []) -> "Builtin.falseValue"
+  | _ -> ("(TODO OO25 expr: " ^ A.show_view expr ^ ")")
+  (* | _ -> failwith ("OO5: OCaml output not implemented for expression: " ^ A.show_view expr) *)
+let process_declaration (decl : A.t) : string = 
+  match A.view decl with
+  | A.N(N.Declaration(N.ConstantDefn), [[],name;[], value]) ->  (
+    match A.view name with
+    | A.FreeVar(_) -> (
+      "let " ^ get_identifier_name name ^ " = " ^ get_ocaml_code value
+    )
+    | _ -> failwith ("OO31: Expecting free variable, got: " ^ A.show_view name)
+  )
+  | _ -> ("(TODO OO32 decl: " ^ A.show_view decl ^ ")")
+  (* | _ -> Fail.failwith ("OO29: OCaml output not implemented for declaration: " ^ A.show_view decl) *)
+
 
 let get_ocaml_code_for_module (module_expr : A.t) : string = 
-  let process_declarations (_decls : A.t list) : string list = 
-    ["(*<decls>*)"]
+  let process_declarations (decls : A.t list) : string list = 
+    List.map process_declaration decls
   in
   match A.view module_expr with
   | A.N(N.ModuleDef, (decls))-> 
@@ -35,6 +66,36 @@ let get_ocaml_code_for_module (module_expr : A.t) : string =
     "\nend)"
   | _ -> failwith ("OO30: Expecting module expression, got: " ^ A.show_view module_expr)
 
+let prelude = 
+  String.concat "\n" [
+  "type dt = ..";
+  "module Builtin = struct";
+  "type dt += C_BuiltinStringType";
+  "let stringType = C_BuiltinStringType";
+  "type dt += C_BuiltinIntType";
+  "let intType = C_BuiltinIntType";
+  "type dt += C_BuiltinBoolType";
+  "let boolType = C_BuiltinBoolType";
+  "type dt += C_BuiltinUnitType";
+  "let unitType = C_BuiltinUnitType";
+  "type dt += C_BuiltinFloatType";
+  "let floatType = C_BuiltinFloatType";
+  "type dt += C_BuiltinTypeType";
+  "let typeType = C_BuiltinTypeType";
+  "type dt += C_BuiltinTrueValue";
+  "let trueValue = C_BuiltinTrueValue";
+  "type dt += C_BuiltinFalseValue";
+  "let falseValue = C_BuiltinFalseValue";
+  "type dt += C_BuiltinUnitValue";
+  "let unitValue = C_BuiltinUnitValue";
+  "type dt += C_BuiltinFloatValue of float";
+  "let floatValue v = C_BuiltinFloatValue v";
+  "type dt += C_BuiltinStringValue of string";
+  "let stringValue v = C_BuiltinStringValue v";
+  "type dt += C_BuiltinIntValue of int";
+  "let intValue v = C_BuiltinIntValue v";
+  "end"
+  ]
 let get_ocaml_code_top_level (files : (string * A.t) list) : string = 
   let output_file (filepath : string) (expr : A.t) : string = 
     let code = get_ocaml_code_for_module expr in
@@ -43,6 +104,7 @@ let get_ocaml_code_top_level (files : (string * A.t) list) : string =
       (get_module_name_from_filepath filepath) 
       code
   in
+  prelude ^ "\n" ^
   String.concat "\n" (List.map (fun (filepath, expr) -> output_file filepath expr) files)
 
 
