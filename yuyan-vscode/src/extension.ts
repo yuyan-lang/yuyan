d6c7a6cb725d1ccc3bbf5c43fb5ca114b2709317
up <zhiboc@andrew.cmu.edu>
2025-08-30 19:09:40 -0400

    save

--- yuyan-vscode/src/extension.ts
+++ yuyan-vscode/src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 import * as path from 'path';
 import { exec } from 'child_process';
 import {
@@ -9,11 +10,34 @@ import {
 } from 'vscode-languageclient/node';
 
 function getTokensInfo(document: vscode.TextDocument): void {
-  
+  let path = document.uri.path;
+  let token_file_path = "./_build/lsp_tokens_info/" + path + ".tokens.json";
+  if (fs.existsSync(token_file_path)) {
+    let tokens = JSON.parse(fs.readFileSync(token_file_path, 'utf8'));
+    return tokens;
+  }
+  return undefined;
+
 }
 
 function startLSP(): void {
-  vscode.languages.registerCompletionItemProvider('istari', {
+  let language_selector = "*";
+  vscode.languages.registerDocumentSemanticTokensProvider(language_selector, {
+    provideDocumentSemanticTokens(
+      document: vscode.TextDocument,
+      token: vscode.CancellationToken
+    ): vscode.ProviderResult<vscode.SemanticTokens> {
+      return undefined;
+    }
+  }, new vscode.SemanticTokensLegend(
+    ["StringConstant",
+      "NumericConstant",
+      "StructureKeyword",
+      "ExpressionKeyword",
+      "UserDefinedOperatorKeyword",
+      "Identifier"]
+  ));
+  vscode.languages.registerCompletionItemProvider(language_selector, {
     provideCompletionItems(
       document: vscode.TextDocument,
       position: vscode.Position,
@@ -23,8 +47,7 @@ function startLSP(): void {
       return undefined;
     }
   }, '/');
-
-  vscode.languages.registerHoverProvider('istari', {
+  vscode.languages.registerHoverProvider(language_selector, {
     provideHover(
       document: vscode.TextDocument,
       position: vscode.Position,
@@ -33,8 +56,7 @@ function startLSP(): void {
       return undefined;
     }
   });
-
-  vscode.languages.registerDocumentSymbolProvider('istari', {
+  vscode.languages.registerDocumentSymbolProvider(language_selector, {
     async provideDocumentSymbols(
       document: vscode.TextDocument,
       token: vscode.CancellationToken
@@ -42,8 +64,7 @@ function startLSP(): void {
       return undefined;
     }
   });
-
-  vscode.languages.registerDefinitionProvider('istari', {
+  vscode.languages.registerDefinitionProvider(language_selector, {
     provideDefinition(
       document: vscode.TextDocument,
       position: vscode.Position,
@@ -59,5 +80,5 @@ export function activate(context: vscode.ExtensionContext): void {
 }
 
 export function deactivate(): void {
-  
+
 }
